b0f1d788cea7b1606493743d88effcddfe77d2f1
Dmitrii Korchemkin <chvrkch@icloud.com>
2023-04-13 07:48:51 -0600

    add range component

---
+++ src/lib/PowerRange.svelte
@@ -0,0 +1,82 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+	import { createEventDispatcher } from 'svelte';
+	import debounce from 'lodash/debounce';
+
+	export let value = 0;
+	export let label = 'Power';
+
+	$: volume = value;
+	const markers = [0, 25, 50, 75, 100];
+
+	function handleInput({ target }: Event) {
+		volume = Number((target as HTMLInputElement)?.value);
+	}
+
+	const dispatch = createEventDispatcher<ComponentEvent>();
+	const handleChange = debounce(() => {
+		dispatch('change', { payload: volume });
+	}, 300);
+</script>
+
+<section in:fade class="range">
+	<label for="volume" class="range__label">
+		{label}: <span class="range__volume">{volume}%</span>
+	</label>
+
+	<input
+		type="range"
+		class="range__input"
+		list="markers"
+		min="0"
+		max="100"
+		bind:value={volume}
+		on:change={handleChange}
+	/>
+
+	<datalist id="markers">
+		{#each markers as marker}
+			<option value={marker} />
+		{/each}
+	</datalist>
+</section>
+
+<style>
+	.range,
+	.range__label {
+		display: grid;
+		place-content: center;
+		place-items: center;
+		gap: 0.5rem;
+	}
+
+	.range {
+		padding: 2rem;
+		border-radius: 1rem;
+		color: var(--dark);
+		background: var(--light);
+		min-width: 250px;
+	}
+
+	.range__label {
+		font-size: 2rem;
+	}
+
+	.range__volume {
+		font-size: 4rem;
+		white-space: nowrap;
+	}
+
+	/* TODO .range__input::-webkit-scrollbar {
+    width: 4px;
+  }
+
+  .range__input::-webkit-scrollbar-thumb {
+    background: red;
+    border-radius: 4px;
+  }
+
+  .range__input::-webkit-scrollbar-thumb:hover {
+    background: red;
+  } */
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import PowerRange from '../lib/PowerRange.svelte';
+	import { onMount } from 'svelte';
+	import mqtt_client from 'u8-mqtt/esm/node/index.js';
+
+	const label = browser && /ru/.test(window.navigator.language) ? 'Мощность' : 'Power';
+	const my_mqtt = mqtt_client();
+
+	function onPowerChange({ detail }: CustomEvent<ComponentEvent>) {
+		my_mqtt.json_send('svelte/power/range', { value: detail.payload });
+	}
+
+	$: value = 25;
+
+	onMount(async () => {
+		my_mqtt.with_websock('wss://test.mosquitto.org:8081').with_autoreconnect();
+
+		await my_mqtt.connect();
+
+		my_mqtt.subscribe_topic('svelte/power/range', (pkt: { json: () => { value: number } }) => {
+			const msg = pkt.json();
+
+			if (value != msg.value) {
+				value = msg.value;
+			}
+		});
+	});
+</script>
+
+<svelte:head>
+	<title>Power Range</title>
+</svelte:head>
+
+<PowerRange {value} {label} on:change={onPowerChange} />
